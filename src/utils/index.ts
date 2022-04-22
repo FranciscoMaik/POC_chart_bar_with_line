be4a5db34269f0839e61a344ff90b785b92a024c
FranciscoMaik <chiconunes3081@gmail.com>
2022-04-22 17:40:01 -0300

    feat: create function teste

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import { transformData } from './transformData';
 import { transformDataLine } from './transformDataLine';
+import { testeFunctions } from './testeFunctions';
 
-export { transformData, transformDataLine };
+export { transformData, transformDataLine, testeFunctions };
---
+++ src/utils/testeFunctions.ts
@@ -0,0 +1,113 @@
+import { IChartDataRequest, IChartData } from '../types';
+
+type IAssistantArrayLine = IChartData;
+
+interface IItemResponse {
+  data: IChartData[];
+  keys: Array<string>;
+  maxValueArray: number;
+}
+
+export const testeFunctions = (data: IChartDataRequest): IItemResponse => {
+  const dataChart: IChartData[] = [];
+  const lineChartData: IAssistantArrayLine[] = [];
+
+  const keys = data.tickersInfo.map(item => item.ticker);
+
+  data.tickersInfo.forEach(item =>
+    item.history.forEach(subItem => {
+      const findExistItem = dataChart.findIndex(
+        dataItem => dataItem.label === subItem.date.split('T')[0]
+      );
+
+      if (findExistItem !== -1) {
+        dataChart[findExistItem] = {
+          ...dataChart[findExistItem],
+          [item.ticker]: Number(subItem.value.toFixed(2)),
+        };
+      } else {
+        dataChart.push({
+          label: subItem.date.split('T')[0],
+          [item.ticker]: Number(subItem.value.toFixed(2)),
+          lineChart: '0',
+        });
+      }
+    })
+  );
+
+  data.projection.forEach(item => {
+    lineChartData.push({
+      label: item.date.split('T')[0],
+      lineChart: item.value.toFixed(2),
+    });
+  });
+
+  dataChart.sort((current, next) => {
+    const timestampDateCurrent = new Date(current.label).getTime();
+    const timestampDateNext = new Date(next.label).getTime();
+
+    if (timestampDateCurrent < timestampDateNext) {
+      return -1;
+    }
+    if (timestampDateCurrent > timestampDateNext) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  dataChart.forEach((item, index) => {
+    const findEqualDate = lineChartData.findIndex(
+      itemLine => itemLine.label === item.label
+    );
+
+    if (findEqualDate !== -1) {
+      dataChart[index] = {
+        ...item,
+        lineChart: lineChartData[findEqualDate].lineChart,
+      };
+    }
+  });
+
+  const getEqualIndexArrayLineAndBar = lineChartData.findIndex(
+    item => item.label === dataChart[dataChart.length - 1].label
+  );
+
+  for (
+    let index = getEqualIndexArrayLineAndBar;
+    index < lineChartData.length;
+    index += 1
+  ) {
+    const element = lineChartData[index];
+
+    dataChart.push(element);
+  }
+
+  dataChart.forEach((item, index) => {
+    const getKeys = Object.keys(item);
+
+    let newObject = { ...item };
+
+    keys.forEach(key => {
+      const findEqualItem = getKeys.findIndex(subKey => subKey === key);
+
+      if (findEqualItem === -1) {
+        newObject = { ...newObject, [key]: 0 };
+      }
+    });
+
+    dataChart[index] = newObject;
+  });
+
+  const maxValueArray =
+    Number(
+      Math.max
+        .apply(
+          null,
+          lineChartData.map(item => Number(item.lineChart))
+        )
+        .toFixed(0)
+    ) + 10;
+
+  return { data: dataChart, keys, maxValueArray };
+};
